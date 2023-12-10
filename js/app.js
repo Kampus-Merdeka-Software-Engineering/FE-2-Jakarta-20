06f91a9c1b291d7e45f2275f212c1211449d2054
maifzs <mailafaizashafira@gmail.com>
2023-12-10 22:38:51 +0700

    update contacts and js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,81 @@
-const bar = document.querySelector('.fa-bars');
-const cross = document.querySelector('#hdcross');
-const headerbar = document.querySelector('.headerbar');
+const bar = document.querySelector(".fa-bars");
+const cross = document.querySelector("#hdcross");
+const headerbar = document.querySelector(".headerbar");
+const form = document.querySelector("form");
 
-bar.addEventListener('click',function(){
-    setTimeout(()=>{
-        cross.style.display = 'block';
-    },200);
-    headerbar.style.right = '0%';
-})
+if (bar) {
+  bar.addEventListener("click", function () {
+    setTimeout(() => {
+      cross.style.display = "block";
+    }, 200);
+    headerbar.style.right = "0%";
+  });
+}
 
-cross.addEventListener('click', function(){
-    cross.style.display = 'none';
-    headerbar.style.right = '-100%';
-})
+if (cross) {
+  cross.addEventListener("click", function () {
+    cross.style.display = "none";
+    headerbar.style.right = "-100%";
+  });
+
+}
 
 const baseApiUrl =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://be-2-jakarta-20-production.up.railway.app/";
+    : "https://be-2-jakarta-20-production.up.railway.app";
 
 const apiRoutes = {
   contacts: `${baseApiUrl}/contacts`,
 };
 
-function submitForm(event) {
-  event.preventDefault();
-  const formData = new FormData(this);
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const subject = document.querySelector("#subject").value;
+    const message = document.querySelector("#message").value;
+
+    const formData = { name, email, subject, message };
+
+    fetch(apiRoutes.contacts, {
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Data submitted successfully
+          alert("Data submitted successfully");
+          resetForm();
+        } else {
+          // Handle errors if any
+          alert("Error submitting data");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Error submitting data");
+      });
+});
+
+function submitForm(e) {
+  // collect element input
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const subject = document.querySelector("#subject").value;
+  const message = document.querySelector("#message").value;
+
+  const formData = { name, email, subject, message };
 
   fetch(apiRoutes.contacts, {
+    headers: {
+      "Content-Type": "Application/json",
+    },
     method: "POST",
-    body: formData,
+    body: JSON.stringify(formData),
   })
     .then((response) => {
       if (response.ok) {
